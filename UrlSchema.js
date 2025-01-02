87de06264b4bc8e46d1a4f549e3a87fb4daadb59
Markel-Bradford <markel.bradford94@gmail.com>
2025-01-01 21:59:53 -0600

    Used try catch blocks to better handle errors. Updated Regex to allow localhost urls

--- UrlSchema.js
+++ UrlSchema.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose')
 
 const urlSchema = new mongoose.Schema({
     original_url: String,
-    short_url: Number
+    short_url: {
+        type: Number,
+    required: true,
+    unique: true, // Ensure unique short_url values
+    validate: {
+      validator: Number.isInteger,
+      message: '{VALUE} is not a valid short_url.'
+    }
+    }
 })
 
 module.exports = mongoose.model('Url', urlSchema)
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
-const regex = /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-zA-Z0-9]+(?:\.[a-zA-Z]{2,})+(?:\/[\w#]+\/?)*$/;
+const regex = /^(https?:\/\/)(localhost|(?:www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(:[0-9]{1,5})?(\/[^\s]*)?$/;
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -36,8 +37,10 @@ app.post('/api/shorturl', async function (req, res) {
   // Use regex to confirm that URL input is valid
   if (!regex.test(url_input)) {
     // If invalid, return json object with invalid url message
-    res.json({error: "Invalid URL"})
-  } else {
+    res.json({error: "invalid url"})
+  } 
+
+  try {
     // Find the Url in the database by matching the input parsed from the body to an original_url 
     const urlInDB = await Url.findOne({original_url: url_input})
 
@@ -56,19 +59,32 @@ app.post('/api/shorturl', async function (req, res) {
       // If Url exists, return original url and short url
       res.json({original_url: urlInDB.original_url, short_url: urlInDB.short_url})
     }
+  } catch (error) {
+    console.error("Error in POST /api/shorturl:", error);
+    return res.status(500).json({ error: "Server error" });
   }
 })
 
 app.get('/api/shorturl/:short_url', async function (req, res) {
-  // Find the url that matches the queried short_url
-  const urlInDB = await Url.findOne({ short_url: req.params.short_url });
+  const shortUrlParam = req.params.short_url
 
+  // Validate that short_url is a number
+  if (!shortUrlParam || isNaN(shortUrlParam)) {
+    return res.status(400).json({ error: 'invalid url' });
+  }
+
+try {
+  // Find the url that matches the queried short_url
+  const urlInDB = await Url.findOne({ short_url: Number(shortUrlParam) });
   // If url is not in DB, return error message
   if (!urlInDB) {
-    res.json({error: "Invalid URL"})
-  } else {
+    res.json({error: "invalid url"})
+  } 
     // If present in DB, redirect to original Url
-    res.redirect(urlInDB.original_url)
+  res.redirect(urlInDB.original_url)
+  } catch (error) {
+    console.error("Error in Get /api/shorturl/:short_url:", error);
+    return res.status(500).json({ error: "Server error" });
   }
 })
 
